bf64c017e45c4a0c1bdfbb3fe7e47560ff6fc771
ShHaWkK <alexandreuzan9@gmail.com>
2024-02-19 12:05:17 +0100

    Update client.py

--- client.py
+++ client.py
@@ -17,8 +17,30 @@ class Client:
     def close(self):
         self.client.close()
 
-if __name__ == "__main__":
+def main():
     host = "127.0.0.1"
     port = 5555
     client = Client(host, port)
-    # Logique de communication avec le serveur
+
+    try:
+        while True:
+            # Recevoir le message du serveur etat du jeu 
+            server_response = client.receive()
+            print(server_response)
+
+            # Vérifier si le jeu est terminé
+            if "Félicitations" in server_response or "Dommage" in server_response:
+                break
+
+            # Demander au joueur de deviner une lettre
+            guess = input("Devinez une lettre: ").strip().lower()
+            if guess and len(guess) == 1:
+                client.send(guess)
+            else:
+                print("Veuillez entrer une seule lettre.")
+    finally:
+        print("Fermeture de la connexion.")
+        client.close()
+
+if __name__ == "__main__":
+    main()
